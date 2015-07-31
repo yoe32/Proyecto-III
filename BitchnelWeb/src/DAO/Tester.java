97a55a35e425858f7631d47f70a7d63d786c526a
Yohel <yoe32ns@hotmail.com>
2015-07-31 10:28:59 -0600

    Web Reborn

---
+++ BitchnelWeb/src/DAO/Tester.java
@@ -0,0 +1,40 @@
+package DAO;
+
+import java.util.Scanner;
+
+import Main.Restaurant;
+
+public class Tester {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		Restaurant res = new Restaurant();
+
+		
+
+		Scanner scan = new Scanner(System.in);	
+		res.setId(0);
+		System.out.println("profile name");
+		res.setName(scan.nextLine());
+		System.out.println("profile email");
+		res.setEmail(scan.nextLine());
+		System.out.println("profile password");
+		res.setPassword(scan.nextLine());
+		System.out.println("profiles description");
+		res.setDescription(scan.nextLine());
+		System.out.println("profile location");
+		res.setLocation(scan.nextLine());
+		System.out.println("profile phone 1");
+		res.setPhonei(scan.nextInt());
+		System.out.println("profile phone 2");
+		res.setPhoneii(scan.nextInt());
+		System.out.println("profile phone 3");
+		res.setPhoneiii(scan.nextInt());
+		System.out.println("profile image");
+		res.setImg(null);
+
+		scan.close();
+	}
+}
